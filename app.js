476202f513bdc075fd1f319f08e98f69f3325b3c
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-03-09 09:13:30 -0500

    added resume, link to KPA

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 const linktoScroll = document.querySelector("#projects");
 const section1 = document.querySelector("#section--1");
 
+
 ///////  Smooth scroll to section
 
 linktoScroll.addEventListener("click", function (e) {
